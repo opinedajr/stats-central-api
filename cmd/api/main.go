658ebe6483a80d2497d760b6d20c0e6ace30e25a
Jose Pineda <pinedajr@gmail.com>
2026-05-14 12:36:01 -0300

    feat(teams): Entrypoints and DI

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,6 +23,21 @@ func main() {
 			tournaments.PUT("/:tournamentId", container.TournamentHandler().Update)
 			tournaments.PATCH("/:tournamentId/status", container.TournamentHandler().UpdateStatus)
 		}
+
+		teams := v1.Group("/teams")
+		{
+			teams.GET("", container.TeamsHandler().List)
+			teams.POST("", container.TeamsHandler().MethodNotAllowed)
+			teams.PUT("", container.TeamsHandler().MethodNotAllowed)
+			teams.DELETE("", container.TeamsHandler().MethodNotAllowed)
+			teams.PATCH("", container.TeamsHandler().MethodNotAllowed)
+
+			teams.GET("/:teamId", container.TeamsHandler().Get)
+			teams.POST("/:teamId", container.TeamsHandler().MethodNotAllowed)
+			teams.PUT("/:teamId", container.TeamsHandler().MethodNotAllowed)
+			teams.DELETE("/:teamId", container.TeamsHandler().MethodNotAllowed)
+			teams.PATCH("/:teamId", container.TeamsHandler().MethodNotAllowed)
+		}
 	}
 
 	log.Fatal(r.Run(":3030"))
